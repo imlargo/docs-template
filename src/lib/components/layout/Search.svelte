cd2d8c1320d3ab4511fe4b83885a749a453dfeff
imlargo <jlargob@unal.edu.co>
2026-09-15 08:36:33 -0500

    feat: add Search component for documentation search functionality

---
+++ src/lib/components/layout/Search.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	import { Search } from '@lucide/svelte';
+	import * as Command from '$lib/components/ui/command/index.js';
+	import { Button } from '$components/ui/button';
+
+	let open = $state(false);
+</script>
+
+
+<Button variant="secondary" class="pr-8" onclick={() => open = true} >
+    Search documentation...
+</Button>
+
+<Command.Dialog bind:open>
+	<Command.Input placeholder="Type a command or search..." />
+	<Command.List>
+		<Command.Empty>No results found.</Command.Empty>
+		<Command.Group heading="Suggestions">
+			<Command.Item>Calendar</Command.Item>
+			<Command.Item>Search Emoji</Command.Item>
+			<Command.Item>Calculator</Command.Item>
+		</Command.Group>
+		<Command.Separator />
+		<Command.Group heading="Settings">
+			<Command.Item>Profile</Command.Item>
+			<Command.Item>Billing</Command.Item>
+			<Command.Item>Settings</Command.Item>
+		</Command.Group>
+	</Command.List>
+</Command.Dialog>
