2c5fbad9b55d0b4c37e5d1687c79709440bbefb7
imlargo <jlargob@unal.edu.co>
2026-09-15 08:20:21 -0500

    feat: enhance NavBar with theme and language selection dropdowns

--- src/lib/components/layout/NavBar.svelte
+++ src/lib/components/layout/NavBar.svelte
@@ -2,8 +2,12 @@
 	import { IsMobile } from '$hooks/is-mobile.svelte';
 	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
 	import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+
 	import { cn } from '$lib/utils.js';
 	import type { HTMLAttributes } from 'svelte/elements';
+	import { Languages, Moon, Sun } from '@lucide/svelte';
+	import { buttonVariants } from '$components/ui/button';
 
 	const isMobile = new IsMobile();
 
@@ -21,18 +25,18 @@
 			label: 'Home',
 			href: '/'
 		},
-        {
+		{
 			label: 'Showcase',
 			href: '/showcase'
 		},
-        {
+		{
 			label: 'Docs',
 			href: '/docs'
 		},
-        {
+		{
 			label: 'Blog',
 			href: '/blog'
-		},
+		}
 	];
 </script>
 
@@ -46,7 +50,9 @@
 					<NavigationMenu.Item>
 						<NavigationMenu.Link>
 							{#snippet child()}
-								<a href={link.href} data-sveltekit-reload class={navigationMenuTriggerStyle()}>{link.label}</a>
+								<a href={link.href} data-sveltekit-reload class={navigationMenuTriggerStyle()}
+									>{link.label}</a
+								>
 							{/snippet}
 						</NavigationMenu.Link>
 					</NavigationMenu.Item>
@@ -55,5 +61,39 @@
 		</NavigationMenu.Root>
 	</div>
 
-	<div class="flex items-center justify-between"></div>
+	<div class="flex items-center justify-between gap-x-2">
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger class={cn(buttonVariants({ variant: 'outline', size: 'icon' }))}>
+				<Moon></Moon>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				<DropdownMenu.Group>
+					<DropdownMenu.Label>Theme</DropdownMenu.Label>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item>
+						<Sun class=""></Sun>
+						<span>Light</span>
+					</DropdownMenu.Item>
+					<DropdownMenu.Item>
+						<Moon class=""></Moon>
+						<span>Dark</span>
+					</DropdownMenu.Item>
+				</DropdownMenu.Group>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger class={cn(buttonVariants({ variant: 'outline', size: 'icon' }))}>
+				<Languages></Languages>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				<DropdownMenu.Group>
+					<DropdownMenu.Label>Languaje</DropdownMenu.Label>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item>English</DropdownMenu.Item>
+					<DropdownMenu.Item>Spanish</DropdownMenu.Item>
+				</DropdownMenu.Group>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	</div>
 </div>
