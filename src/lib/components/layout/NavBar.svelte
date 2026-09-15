f96cc517e7ea27f2c8823170c1dd8b6ae6f478c5
imlargo <jlargob@unal.edu.co>
2026-09-14 23:00:59 -0500

    feat: add NavBar component and integrate it into layout

---
+++ src/lib/components/layout/NavBar.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { IsMobile } from '$hooks/is-mobile.svelte';
+	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
+	import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
+	import { cn } from '$lib/utils.js';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	const isMobile = new IsMobile();
+
+	type Link = {
+		label: string;
+		href: string;
+	};
+
+	type Props = {};
+
+	const {} = $props();
+
+	const links: Link[] = [
+		{
+			label: 'Home',
+			href: '/'
+		},
+        {
+			label: 'Showcase',
+			href: '/showcase'
+		},
+        {
+			label: 'Docs',
+			href: '/docs'
+		},
+        {
+			label: 'Blog',
+			href: '/blog'
+		},
+	];
+</script>
+
+<div class="flex w-full items-center justify-between">
+	<div class="flex items-center justify-between gap-x-8">
+		<div>docs</div>
+
+		<NavigationMenu.Root viewport={isMobile.current}>
+			<NavigationMenu.List class="flex-wrap">
+				{#each links as link}
+					<NavigationMenu.Item>
+						<NavigationMenu.Link>
+							{#snippet child()}
+								<a href={link.href} data-sveltekit-reload class={navigationMenuTriggerStyle()}>{link.label}</a>
+							{/snippet}
+						</NavigationMenu.Link>
+					</NavigationMenu.Item>
+				{/each}
+			</NavigationMenu.List>
+		</NavigationMenu.Root>
+	</div>
+
+	<div class="flex items-center justify-between"></div>
+</div>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -3,10 +3,22 @@
 	import AppSidebar from '$lib/components/layout/sidebar/AppSidebar.svelte';
 	import SiteHeader from '$lib/components/layout/sidebar/SiteHeader.svelte';
 	import type { LayoutProps } from './$types';
+	import NavBar from '$components/layout/NavBar.svelte';
 
 	let { children }: LayoutProps = $props();
 </script>
 
+<div class="@container/main flex min-h-0 flex-1 flex-col overflow-auto">
+	<div class="px-4 py-4 border-b md:px-8">
+		<NavBar></NavBar>
+	</div>
+
+	<div id="main-content" class="flex min-h-full flex-1 flex-col px-4 pt-4 pb-8 md:px-8 md:pt-6">
+		{@render children()}
+	</div>
+</div>
+
+<!--
 <Sidebar.Provider
 	class="h-screen"
 	style="--sidebar-width: calc(var(--spacing) * 64); --header-height: calc(var(--spacing) * 12);"
@@ -21,3 +33,4 @@
 		</div>
 	</Sidebar.Inset>
 </Sidebar.Provider>
+-->
